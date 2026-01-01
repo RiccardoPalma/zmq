4171f942e288c7930113f4dfa367bed90c019cc2
RiccardoPalma <palmariccardog@gmail.com>
2026-01-01 23:14:16 +0100

    first

---
+++ build/CMakeFiles/sub_b.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sub_b.dir/sub_b.cpp.o"
+  "CMakeFiles/sub_b.dir/sub_b.cpp.o.d"
+  "sub_b"
+  "sub_b.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sub_b.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ sub_a.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "cppzmq/zmq.hpp"
+
+int main() {
+    zmq::context_t ctx;
+    zmq::socket_t subscriber(ctx, zmq::socket_type::sub);
+    subscriber.connect("tcp://127.0.0.1:5555");
+
+    subscriber.set(zmq::sockopt::subscribe, "temperature");
+
+    for (int i = 0; i < 5; ++i) {
+        zmq::message_t msg;
+        subscriber.recv(msg, zmq::recv_flags::none);
+
+        std::string data(static_cast<char*>(msg.data()), msg.size());
+        std::cout << "Ricevuto: " << data << std::endl;
+    }
+
+    return 0;
+}
