b90b6c48da3f252ea24b5a64a7b66d4081b03a5e
RiccardoPalma <palmariccardog@gmail.com>
2026-01-02 12:25:53 +0100

    fatto

---
+++ build/CMakeFiles/test_suba.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_suba.dir/test/sub_a.cpp.o"
+  "CMakeFiles/test_suba.dir/test/sub_a.cpp.o.d"
+  "test_suba"
+  "test_suba.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_suba.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/test_subb.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_subb.dir/test/sub_b.cpp.o"
+  "CMakeFiles/test_subb.dir/test/sub_b.cpp.o.d"
+  "test_subb"
+  "test_subb.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_subb.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
