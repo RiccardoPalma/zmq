b12215c2503ed1d77373e424a7e6d4598c5d1acc
palmariccardo@gmail.com <palmariccardo@gmail.com>
2026-01-15 15:12:50 +0100

    better stop

--- zmq_broker.cpp
+++ zmq_broker.cpp
@@ -1,26 +1,33 @@
-#include <zmq.hpp>
 #include <csignal>
-#include <atomic>
 #include <iostream>
+#include <zmq.hpp>
 
-static std::atomic<bool> running{true};
-void signal_handler(int){ running = false; }
+zmq::context_t ctx(1);
+
+void signal_handler(int) { ctx.shutdown(); }
 
 int main() {
-    std::signal(SIGINT, signal_handler);
-    std::signal(SIGTERM, signal_handler);
+  std::signal(SIGINT, signal_handler);
+  std::signal(SIGTERM, signal_handler);
 
-    zmq::context_t ctx(1);
-    zmq::socket_t xsub(ctx, zmq::socket_type::xsub);
-    zmq::socket_t xpub(ctx, zmq::socket_type::xpub);
+  zmq::socket_t xsub(ctx, zmq::socket_type::xsub);
+  zmq::socket_t xpub(ctx, zmq::socket_type::xpub);
 
-    xsub.bind("tcp://127.0.0.1:5555"); // ingress: PUB
-    xpub.bind("tcp://127.0.0.1:5556"); // egress: SUB
+  xsub.bind("tcp://127.0.0.1:5555");
+  xpub.bind("tcp://127.0.0.1:5556");
 
-    std::cout << "[ZMQ] Broker started\n";
+  std::cout << "[ZMQ] Broker started\n";
 
+  try {
     zmq::proxy(xsub, xpub);
+  } catch (const zmq::error_t &e) {
+    if (e.num() == EINTR) {
+      std::cout << "\n[ZMQ] Broker interrupted\n";
+    } else {
+      // crash and restart
+    }
+  }
 
-    std::cout << "[ZMQ] Broker stopped\n";
-    return 0;
+  std::cout << "[ZMQ] Broker stopped\n";
+  return 0;
 }
